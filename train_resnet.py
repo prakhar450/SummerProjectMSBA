bff5ca2a5a45710aa00f94b159857c24c1a95e0a
Prakhar Srivastav <prakhar.bits14@gmail.com>
2023-10-10 01:47:45 -0700

    fixed dataloader

--- train_resnet.py
+++ train_resnet.py
@@ -187,7 +187,7 @@ class ResNet(nn.Module):
         self.layer2 = self.make_layer(block, 32, layers[1], 2)
         self.layer3 = self.make_layer(block, 64, layers[2], 2)
         self.avg_pool = nn.AvgPool2d(16)
-        self.fc = nn.Linear(64, num_classes)
+        self.fc = nn.Linear(576, num_classes)
         
     def make_layer(self, block, out_channels, blocks, stride=1):
         downsample = None
